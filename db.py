61c6d209b61f4ab0c87bd715a083bda779a75e2f
Francisco García Delgado <frangardel@gmail.com>
2022-10-28 21:28:03 +0200

    Create database model

--- db.py
+++ db.py
@@ -18,6 +18,57 @@ tables = conn.execute(
  "SHOW TABLES;"
 )
 
+create_sql = """
+    CREATE TABLE vecino (
+            id int,
+            name varchar(255),
+            email varchar(255),
+            status varchar(10)
+);
+"""
+
+conn.execute(create_sql)
+
+create_sql = """
+    CREATE TABLE resource (
+            id int,
+            type varchar(255),
+            max_pax int,
+            price float,
+            id_timetable int,
+            in_advance int
+);
+"""
+
+conn.execute(create_sql)
+
+create_sql = """
+    CREATE TABLE timetable (
+            id int,
+            start time,
+            end time
+);
+"""
+
+conn.execute(create_sql)
+
+create_sql = """
+    CREATE TABLE reservation (
+            id int,
+            id_resource int,
+            id_vecino int,
+            status varchar(10),
+            start time,
+            num_pax int
+);
+"""
+
+conn.execute(create_sql)
+
+tables = conn.execute(
+ "SHOW TABLES;"
+)
+
 for table in tables.fetchall():
     print(table)
 
